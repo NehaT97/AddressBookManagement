1b06b67397401e79bed8c3319f3ee95828538c5c
unknown <nehathakur764@gmail.com>
2020-11-21 08:45:03 +0530

    Edit The Person details

--- src/com/AddressBookManagement/AddressBook.java
+++ src/com/AddressBookManagement/AddressBook.java
@@ -8,7 +8,7 @@ public class AddressBook {
     public static ArrayList<Person> addressbooklist = new ArrayList<Person>();
 
     /*UC2: Add Person Details In AddressBook*/
-    private void addPerson()
+    public void addPerson()
     {
         System.out.println("____Add Person Details____");
         System.out.println("\nEnter the FirstName");
@@ -41,12 +41,86 @@ public class AddressBook {
 
     }
 
+    /* UC2: Edit Person Details*/
+    public void editPerson()
+    {
+        System.out.println("\n___Edit the Person Using Name___");
+        System.out.println("\nEnter the FirstName And LastName To Find Records.\nFirstName");
+        String firstName=sc.nextLine();
+        System.out.println("\nLastName");
+        String lastName = sc.nextLine();
+
+        int loop=1;
+        for (Person person : addressbooklist)
+        {
+            if (person.getFirstname().equals(firstName)  &&  person.getLastname().equals(lastName))
+            {
+                System.out.println("\nData Found as per match!!!");
+                System.out.println( person.toString());
+                while (loop==1)
+                {
+                    System.out.println("\n\n Enter what field you want to edit(address/contact/emailid/quit) : ");
+                    String field = sc.nextLine(); //taken input as address or contact
+
+                    if (field.equals("address")) {
+                        System.out.println("\n Enter Address : To Edit ");
+
+                        System.out.print("\b\n City : ");
+                        String city = sc.nextLine();
+
+                        System.out.print("\b\n State : ");
+                        String state = sc.nextLine();
+
+                        System.out.print("\n ZipCode: ");
+                        String zip = sc.nextLine();
+
+                        //Adding Edited Address Related Data To Address arraylist
+                        person.setCity(city);
+                        person.setState(state);
+                        person.setZip(zip);
+
+                        System.out.println("address Edited Successfully:" +toString());
+                    }
+
+                    else if (field.equals("contact")) {
+                        System.out.print("\n Enter contact number : ");
+                        String contact = sc.nextLine();
+                        person.setContactno(contact);
+                        System.out.println("\nContact Edited Successfully :" + person.toString());
+
+                    }
+
+                    else if (field.equals("emailid")){
+                        System.out.println("Enter email id");
+                        String emailid = sc.nextLine();
+                        person.setEmailid(emailid);
+                        System.out.println("\nContact Edited Successfuly :" + person.toString());
+
+                    }
+
+                    else if (field.equals("quit")) {
+                        loop = 0;
+                        System.out.println("\nExit From Edit Functionality");
+                    }
+
+                }
+
+            }
+            else
+            {
+                System.out.println("given Wrong Input : Unable to find data!!!!");
+
+            }
+        }
+    }
+
+
     public static void main(String []args)
     {
         AddressBook addressBook=new AddressBook();
         System.out.println("WELCOME TO ADDRESS BOOK MANAGEMENT");
         addressBook.addPerson();
-
+        addressBook.editPerson();
     }
 
 
