7e6168e13874c8b01a13a104dc8ca8d93ba54509
unknown <nehathakur764@gmail.com>
2020-11-21 08:13:14 +0530

    Add New Person Details In AddressBook

--- src/com/AddressBookManagement/AddressBook.java
+++ src/com/AddressBookManagement/AddressBook.java
@@ -3,11 +3,49 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
+
+    Scanner sc=new Scanner(System.in);
+    public static ArrayList<Person> addressbooklist = new ArrayList<Person>();
+
+    /*UC2: Add Person Details In AddressBook*/
+    private void addPerson()
+    {
+        System.out.println("____Add Person Details____");
+        System.out.println("\nEnter the FirstName");
+        String firstname = sc.nextLine();
+
+        System.out.println("Enter the LastName");
+        String lastname = sc.nextLine();
+
+        System.out.println("Enter the Contact");
+        String contactno = sc.nextLine();
+
+        System.out.println("Enter the Emailid");
+        String emailid = sc.nextLine();
+
+        System.out.println("Enter the Address");
+        String address = sc.nextLine();
+
+        System.out.println("Enter the city");
+        String city = sc.nextLine();
+
+        System.out.println("Enter the state");
+        String state = sc.nextLine();
+
+        System.out.println("Enter the zip");
+        String zip = sc.nextLine();
+
+        Person p=new Person(firstname,lastname,contactno,emailid,address,city,state,zip);
+        addressbooklist.add(p);
+        System.out.println("AddressBook Data:" +addressbooklist);
+
+    }
+
     public static void main(String []args)
     {
         AddressBook addressBook=new AddressBook();
         System.out.println("WELCOME TO ADDRESS BOOK MANAGEMENT");
-
+        addressBook.addPerson();
 
     }
 
